e515bc81655eafa341f47e5c7860723ed259b522
Charles German <5donuts@pm.me>
2024-05-24 14:31:36 -0400

    Add config for ACM certificate

--- main.tf
+++ main.tf
@@ -54,3 +54,54 @@ resource "aws_route53_record" "alias" {
     evaluate_target_health = each.value.alias.eval_target_health
   }
 }
+
+# Records to validate the ACM certificate used by the site
+resource "aws_route53_record" "acm_validation" {
+  for_each = {
+    for option in aws_acm_certificate.site.domain_validation_options : option.domain_name => {
+      name   = option.resource_record_name
+      type   = option.resource_record_type
+      record = option.resource_record_value
+    }
+  }
+
+  zone_id = aws_route53_zone.site.zone_id
+
+  name = each.value.name
+  type = each.value.type
+  ttl  = 60
+  records = [
+    each.value.record
+  ]
+}
+
+# --------------------------------------------------------------------------- #
+#                Configure the ACM certificate for the website                #
+# --------------------------------------------------------------------------- #
+
+# ACM Certificates used by CloudFront _must_ be issued from the us-east-1 region.
+# See: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/cnames-and-https-requirements.html
+resource "aws_acm_certificate" "site" {
+  provider = aws.us_east_1
+
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+  key_algorithm     = "RSA_2048" # I kept getting SSL_ERROR_NO_CYPHER_OVERLAP errors with ECDSA ciphers...
+
+  subject_alternative_names = [
+    "www.${var.domain_name}"
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# This resource does not represent a real-word entity.
+# This resource is used to wait for ACM validation to complete.
+resource "aws_acm_certificate_validation" "site" {
+  provider = aws.us_east_1
+
+  certificate_arn         = aws_acm_certificate.site.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
+}
